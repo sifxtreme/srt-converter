1245a6bd9dd4e8aa58e5bf2b8a093fa9af123a36
Asif Ahmed <asif.h.ahmed@gmail.com>
2025-01-11 19:58:26 -0800

    Add PM2 for Production Build

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,21 @@
+export default {
+  apps: [{
+    name: "srt-translator",
+    script: "./server.js",
+    instances: "max",
+    exec_mode: "cluster",
+    autorestart: true,
+    watch: false,
+    max_memory_restart: "1G",
+    log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+    error_file: "./logs/error.log",
+    out_file: "./logs/out.log",
+    merge_logs: true,
+    env: {
+      NODE_ENV: "development"
+    },
+    env_production: {
+      NODE_ENV: "production"
+    }
+  }]
+}
